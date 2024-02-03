90cdae0110da6a8ed697c0eb8229360b057da179
boram <5018b@naver.com>
2024-02-03 14:58:33 +0900

    프로그래머스 | 행렬의 곱셈

---
+++ Programmers/src/main/java/Problem12949.java
@@ -0,0 +1,16 @@
+class Problem12949 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        //arr1행 arr2열
+
+        for(int i=0; i<answer.length; i++) {
+            for(int j=0; j<answer[0].length; j++) {
+                for(int k=0; k<arr1[0].length; k++) {
+                    answer[i][j] += (arr1[i][k] * arr2[k][j]);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
