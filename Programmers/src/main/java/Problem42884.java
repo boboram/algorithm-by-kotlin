81854aada71435f5f12afb45848b147e9b34b057
boram <5018b@naver.com>
2024-02-06 23:32:09 +0900

    프로그래머스 | 단속카메라 > 주석 추가

--- Programmers/src/main/java/Problem42884.java
+++ Programmers/src/main/java/Problem42884.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
+/**
+ * 단속카메라
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java
+ */
 public class Problem42884 {
     public int solution(int[][] routes) {
 
+        //진출지점 기준으로 내림자순
         Arrays.sort(routes, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
